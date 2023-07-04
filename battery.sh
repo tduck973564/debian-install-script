0c4d77c5d0b04035925e8dc5a992bf9055265a29
tduck973564 <tduck973564@gmail.com>
2023-07-04 18:40:08 +1000

    initial

---
+++ battery.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Optimizes battery
+sudo apt remove power-profiles-daemon
+
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
+sudo systemctl enable --now NetworkManager-dispatcher
+
+sudo apt install -y tlp tlp-rdw powertop
+
+sudo sh -c "echo 'PCIE_ASPM_ON_BAT=powersupersave
+PLATFORM_PROFILE_ON_BAT=low-power
+NMI_WATCHDOG=0
+CPU_PERF_POLICY_ON_BAT=power
+DEVICES_TO_DISABLE_ON_STARTUP=\"bluetooth nfc wwan\"
+DEVICES_TO_ENABLE_ON_STARTUP=\"wifi\"
+RADEON_DPM_PERF_LEVEL_ON_BAT=auto
+RADEON_DPM_STATE_ON_BAT=battery
+CPU_SCALING_GOVERNOR_ON_BAT=schedutil
+CPU_BOOST_ON_BAT=0
+' >> /etc/tlp.conf"
+
+sudo systemctl enable --now tlp
+sudo tlp-rdw enable
+
+sudo powertop --auto-tune
+sudo systemctl enable --now powertop
---
+++ script.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Fedora Install Script by tduck973564
+
+echo "CD into home directory"
+cd ~
+
+echo "Enable contrib"
+
+sudo sh -c "echo '# See https://wiki.debian.org/SourcesList for more information.
+deb http://deb.debian.org/debian bookworm main non-free-firmware non-free contrib
+deb-src http://deb.debian.org/debian bookworm main non-free-firmware non-free contrib
+
+deb http://deb.debian.org/debian bookworm-updates main non-free-firmware non-free contrib
+deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware non-free contrib
+
+deb http://security.debian.org/debian-security/ bookworm-security main non-free-firmware non-free contrib
+deb-src http://security.debian.org/debian-security/ bookworm-security main non-free-firmware non-free contrib
+
+# Backports allow you to install newer versions of software made available for this release
+deb http://deb.debian.org/debian bookworm-backports main non-free-firmware non-free contrib
+deb-src http://deb.debian.org/debian bookworm-backports main non-free-firmware non-free contrib
+' > /etc/apt/sources.list"
+sudo apt update
+
+echo "Install flatpak"
+sudo apt install -y flatpak plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt remove firefox-esr
+flatpak install -y org.mozilla.firefox
+
+echo "Installation of GitHub CLI and setup of Git"
+sudo apt install -y gh
+sh -c "gh auth login"
+
+echo "Type in your git username: "
+read GITUSERNAME
+echo "Type in your git email: "
+read GITEMAIL
+
+git config --global user.name $GITUSERNAME
+git config --global user.email $GITEMAIL
+
+echo "Installation of build-essential"
+sudo apt install build-essential
+
+echo "Installation of apps"
+sudo apt remove -y ibus
+
+sudo apt install -y \
+pavucontrol \
+openssl \
+libdvd-pkg
+
+sudo apt install -y libavcodec-extra \
+  gstreamer1.0-libav              \
+  gstreamer1.0-plugins-ugly       \
+  gstreamer1.0-vaapi;
+
+flatpak install -y flathub \
+com.github.tchx84.Flatseal \
+org.gimp.GIMP \
+org.kde.kdenlive \
+org.inkscape.Inkscape \
+com.github.wwmm.easyeffects \
+org.mozilla.firefox
+
+echo "Make some folders"
+mkdir ~/Repositories
+mkdir ~/Coding
+
+echo "Download icon theme and fonts"
+sudo apt install -y fonts-inter fonts-inter-variable fonts-ibm-plex
+
+echo "Install AppImageLauncher"
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:appimagelauncher-team/stable
+sudo apt update
+sudo apt install -y appimagelauncher
+
+sudo apt autoremove -y
+sudo apt update
+echo -e '\nDone!'
